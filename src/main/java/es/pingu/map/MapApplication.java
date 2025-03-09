1cb7c07d49a55047b5ca4ab7437d0e4d5494c515
Nicolás Barona Riera <nbaronariera@gmail.com>
2025-03-09 11:00:16 +0100

    Test: Tremendo contenido

--- src/main/java/es/pingu/map/MapApplication.java
+++ src/main/java/es/pingu/map/MapApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MapApplication {
 
+	// Testeo testeillo
 	public static void main(String[] args) {
 		SpringApplication.run(MapApplication.class, args);
 	}
