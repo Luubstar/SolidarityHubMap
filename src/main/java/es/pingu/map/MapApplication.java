4b4c37431033c850abd93224cca7d5124bab4e68
Nube <jesusjaveacg@gmail.com>
2025-03-09 11:19:36 +0100

    ADD new comment

--- src/main/java/es/pingu/map/MapApplication.java
+++ src/main/java/es/pingu/map/MapApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MapApplication {
 
-	// Testeo testeillo
+	// Testeo testeillo version 2
 	public static void main(String[] args) {
 		SpringApplication.run(MapApplication.class, args);
 	}
