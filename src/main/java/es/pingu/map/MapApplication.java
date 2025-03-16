300a32ad1567ef653588e8e3d28f8bc5ca5e1066
Nicolás Barona Riera <nbaronariera@gmail.com>
2025-03-16 10:37:45 +0100

    Ahora no va el mapa?

--- src/main/java/es/pingu/map/MapApplication.java
+++ src/main/java/es/pingu/map/MapApplication.java
@@ -1,10 +1,14 @@
 package es.pingu.map;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.spring.annotation.EnableVaadin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MapApplication {
+@EnableVaadin
+public class MapApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MapApplication.class, args);
