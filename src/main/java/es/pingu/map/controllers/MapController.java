69f4103681f72ee2f433e5733af518f9d0f3fca9
FranBlanco2003 <17frablamig@gmail.com>
2025-03-20 20:42:24 +0100

    *Se pueden crear marcadores con el boton y controlar el click*

--- src/main/java/es/pingu/map/controllers/MapController.java
+++ src/main/java/es/pingu/map/controllers/MapController.java
@@ -1,45 +1,53 @@
 package es.pingu.map.controllers;
 
+import com.vaadin.flow.component.ClientCallable;
+import com.vaadin.flow.component.UI;
+import elemental.json.JsonObject;
+import elemental.json.JsonValue;
+import es.pingu.map.views.MapView;
 import jakarta.validation.constraints.NotNull;
 import lombok.NonNull;
 import software.xdev.vaadin.maps.leaflet.basictypes.LLatLng;
 import software.xdev.vaadin.maps.leaflet.layer.raster.LTileLayer;
+import software.xdev.vaadin.maps.leaflet.layer.ui.LMarker;
 import software.xdev.vaadin.maps.leaflet.map.LMap;
+import software.xdev.vaadin.maps.leaflet.map.LMapLocateOptions;
 import software.xdev.vaadin.maps.leaflet.registry.LComponentManagementRegistry;
+
+import java.util.ArrayList;
 
 public class MapController {
     private LComponentManagementRegistry reg;
     private LMap map;
+    private LLatLng coords;
+    private ArrayList<LLatLng> points;
 
-    private boolean creatingZone;
-    private boolean creatingTask;
 
-    public MapController(@NotNull LComponentManagementRegistry reg, @NonNull LMap map, double lat, double lon) {
+
+    public MapController(@NotNull LComponentManagementRegistry reg, @NonNull LMap map) {
         this.reg = reg;
         this.map = map;
-
-        this.map.addLayer(LTileLayer.createDefaultForOpenStreetMapTileServer(this.reg));
-        this.map.setView(new LLatLng(reg, lat, lon), 13);
-        this.SetMapEvent();
     }
 
-    private void SetMapEvent(){
-        this.map.on(
-                "click",
-                "e => alert('Click en Lat -> ' + e.latlng.lat + ', Lon -> ' + e.latlng.lng)");
-    }
 
+    // TODO: Texto para el el marcador de tarea
     public void createTask(){
-        this.creatingZone = false;
-        this.creatingTask = true;
         // TODO: ¿Modificar el mapa? realmente no tengo ni idea
         // TODO: ¿Cambiar cursor?
+        System.out.println("Task created at: " + coords);
+        new LMarker(reg,coords).addTo(map);
+        UI.getCurrent();
+
     }
 
     public void createZone(){
-        this.creatingTask = false;
-        this.creatingZone = true;
         // TODO: ¿Modificar el mapa? realmente no tengo ni idea
         // TODO: ¿Cambiar cursor?
     }
+
+    public void setCoords(double lat, double lng) {
+        this.coords = new LLatLng(reg, lat, lng);
+    }
+
+
 }
--- src/main/java/es/pingu/map/views/MapView.java
+++ src/main/java/es/pingu/map/views/MapView.java
@@ -1,14 +1,24 @@
 package es.pingu.map.views;
 
+import com.vaadin.flow.component.ClientCallable;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import elemental.json.JsonObject;
+import elemental.json.JsonValue;
 import es.pingu.map.commons.NavigationBar;
+import es.pingu.map.controllers.MapControllTypes;
 import es.pingu.map.controllers.MapController;
 import software.xdev.vaadin.maps.leaflet.MapContainer;
+import software.xdev.vaadin.maps.leaflet.basictypes.LLatLng;
+import software.xdev.vaadin.maps.leaflet.layer.raster.LTileLayer;
 import software.xdev.vaadin.maps.leaflet.map.LMap;
+import software.xdev.vaadin.maps.leaflet.map.LMapLocateOptions;
 import software.xdev.vaadin.maps.leaflet.registry.LComponentManagementRegistry;
 import software.xdev.vaadin.maps.leaflet.registry.LDefaultComponentManagementRegistry;
 
@@ -17,8 +27,15 @@ import software.xdev.vaadin.maps.leaflet.registry.LDefaultComponentManagementReg
 public class MapView extends HorizontalLayout {
 
     MapController controller;
+    private final String ID = "mapa";
+    private final LMap map;
+    private final Object lock = new Object();
+    private UI ui;
+
+
 
     public MapView() {
+        this.setId(ID);
         this.setSizeFull();
         this.add(NavigationBar.createNavBar());
 
@@ -30,7 +47,11 @@ public class MapView extends HorizontalLayout {
         final LComponentManagementRegistry reg = new LDefaultComponentManagementRegistry(this);
         final MapContainer mapContainer = new MapContainer(reg);
         mapContainer.setSizeFull();
-        final LMap map = mapContainer.getlMap();
+        this.map = mapContainer.getlMap();
+        this.map.addLayer(LTileLayer.createDefaultForOpenStreetMapTileServer(reg));
+        this.map.locate(new LMapLocateOptions().withSetView(true));
+
+
 
         MapVerticalLayout.add(mapContainer);
         MapVerticalLayout.add(ButtonLayout);
@@ -39,9 +60,58 @@ public class MapView extends HorizontalLayout {
         Button zona = new Button("Zona");
         ButtonLayout.add(tarea, zona);
 
-        controller = new MapController(reg, map, 39.47, -0.42);
+        controller = new MapController(reg, map);
 
-        tarea.addClickListener(e -> controller.createTask());
-        zona.addClickListener(e -> controller.createZone());
+        tarea.addClickListener(e -> click(MapControllTypes.TAREA));
+        zona.addClickListener(e -> click(MapControllTypes.ZONA));
+
+
     }
+
+    public void click(MapControllTypes Action) {
+        this.map.once("click", "e => document.getElementById('" + ID + "').$server.mapClicked(e.latlng)");
+
+        ui = UI.getCurrent();
+
+        new Thread(() -> {
+
+            synchronized (lock) {
+                try {
+                    System.out.println("Esperando clic en el mapa...");
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Erros esperando clic en el mapa"+e);
+                    return;
+                }
+            }
+            if (ui != null) {
+                ui.access(() -> {
+                    switch (Action) {
+                        case TAREA:
+                            controller.createTask();
+                            break;
+                        case ZONA:
+                            controller.createZone();
+                            break;
+                    }
+                });
+            }
+        }).start();
+    }
+
+    @ClientCallable
+    public void mapClicked(final JsonValue input) {
+        if (!(input instanceof final JsonObject obj)) {
+            return;
+        }
+
+        controller.setCoords(obj.getNumber("lat"), obj.getNumber("lng"));
+
+        synchronized (lock) {
+            lock.notify();
+        }
+
+        ui.push();
+    }
+
 }
